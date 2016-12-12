970469247bbf00387b63cfe83fcff6623186996a
anonpen <sand.boooox@gmail.com>
2016-12-12 13:52:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,25 @@
       <p class="bar_over"></p>
       <p class="bar_under"></p>
     </div>
+<?php
 
+$link = mysql_connect('us-cdbr-iron-east-04.cleardb.net:3306', 'b56f633bf4a00f', 
+
+'079a2d1a');
+if (!$link) {
+    die('接続失敗です。'.mysql_error());
+}
+
+print('<p>接続に成功しました。</p>');
+
+// MySQLに対する処理
+
+$close_flag = mysql_close($link);
+
+if ($close_flag){
+    print('<p>切断に成功しました。</p>');
+}
+
+?>
   </body>
 </html>
